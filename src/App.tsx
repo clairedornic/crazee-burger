807c3f3f0cfb7575b0544b2229cbc42d5a9ca48c
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-03 18:18:00 +0200

    refactor: improve components structure and name

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css'
-import { Login } from './pages/Login'
+import { LoginPage } from './pages/LoginPage'
 
 function App() {
 
   return (
     <>
       <div>
-        <Login></Login>
+        <LoginPage></LoginPage>
       </div>
     </>
   )
