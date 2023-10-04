8f2a92b9ce3d280bebe24fe064ffe5a2e984981c
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-04 13:23:37 +0200

    refactor: change the type of routing browser

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { LoginPage } from './components/pages/login/LoginPage.tsx'
-import { ErrorPage } from './components/pages/ErrorPage.tsx'
-import { OrderPage } from './components/pages/OrderPage.tsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LoginPage></LoginPage>,
-    errorElement: <ErrorPage></ErrorPage> 
-  },
-  {
-    path: "/order",
-    element: <OrderPage />,
-  },
-]);
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
