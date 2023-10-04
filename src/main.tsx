d186684db4e65ab4ea049c8aaaefa0c6c9ead6a0
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-04 12:22:02 +0200

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { LoginPage } from './components/pages/login/LoginPage.tsx'
+import { ErrorPage } from './components/pages/ErrorPage.tsx'
+import { OrderPage } from './components/pages/OrderPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage></LoginPage>,
+    errorElement: <ErrorPage></ErrorPage> 
+  },
+  {
+    path: "/order",
+    element: <OrderPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
