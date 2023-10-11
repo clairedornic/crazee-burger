8d10f26ea9951bb245daaabeac17c8513cc30f75
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-11 11:37:52 +0200

    style<theme>: add properties

---
+++ src/theme/theme-design.js
@@ -0,0 +1,38 @@
+const theme = {
+    colors: {
+      white: '#fff',
+      gray: '#747B91',
+      lightgray: '#F5F5F7',
+      black: '#000',
+      primary: '#FF9F1B',
+      logo: 'FFA01B',
+      secondary: '#F56A2C',
+      red:'#FF0000'
+    },
+    spacing: {
+      extralarge: '30px',
+      large: '24px',
+      medium: '18px',
+      small: '12px',
+      extrasmall: '6px',
+      none: '0',
+    },
+    fontsize: {
+      extralarge: '48px',
+      large: '36px',
+      medium: '16px',
+      regular: '15px',
+      small: '14px',
+      extrasmall: '12px',
+      thin: '10px',
+    },
+    size: {
+      full: '100%',
+    },
+    radius: {
+      regular: '0.3125rem',
+    },
+  };
+  
+  export default theme;
+  
