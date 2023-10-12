7ae52c81a168f36672c141ab6feca3b3874aded2
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-12 18:25:27 +0200

    feat: add navbar component and added it to OrderPage

---
+++ src/components/pages/order/Navbar.tsx
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import { BsPersonCircle } from 'react-icons/bs';
+import { Link } from "react-router-dom"
+import { Logo } from '../../../components/reusable-ui/Logo';
+import { theme } from "../../../assets/styles/theme/theme-design";
+
+interface NavBarProps {
+    userName: string;
+}
+
+export const Navbar: React.FC<NavBarProps> = ({userName}) => {
+
+    const refreshPage = () => {
+        window.location.reload();
+    }
+
+  return (
+    <NavbarStyled>
+        <button onClick={refreshPage} className='logo'>
+            <Logo/>
+        </button>
+        <div className='profil'>
+           <div className='infos-cta'>
+                <p>Hey, <span>{userName}</span></p>
+                <Link to={`/`}>Se déconnecter</Link>
+            </div>
+            <BsPersonCircle className="icon"/>
+        </div>
+    </NavbarStyled>
+  )
+}
+
+const NavbarStyled = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 70px 20px 20px;
+    background-color: ${theme.colors.white};
+    border-radius: ${theme.borderRadius.round} ${theme.borderRadius.round} 0 0;
+    
+    .logo {
+        border: none;
+        background: none;
+        
+        &>div {
+            cursor: pointer;
+        }
+
+        p {
+            font-size: ${theme.fonts.P4};
+        }
+
+        img {
+            max-width: 80px;
+            margin: 0 5px;
+        }
+    }
+
+    .profil {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+
+        .infos-cta {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            gap: 4px;
+            color: ${theme.colors.gray};
+
+            p {
+                font-size: 16px;
+                margin: 0;
+            }
+
+            span {
+                font-weight: ${theme.weights.bold};
+                color: ${theme.colors.primary};
+            }
+
+            a {
+                font-size: ${theme.fonts.XXS};
+
+                &:active,
+                &:visited {
+                    color: ${theme.colors.gray};
+                }
+            }
+        }
+
+        .icon {
+            color: ${theme.colors.gray};
+            width: 36px;
+            height: 36px;
+        }
+    }
+`;
--- src/components/pages/order/OrderPage.tsx
+++ src/components/pages/order/OrderPage.tsx
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom"
 import { useLocation } from "react-router-dom";
+import styled from 'styled-components';
+import { theme } from "../../../assets/styles/theme/theme-design";
+import { Navbar } from "./Navbar";
 
 export const OrderPage = () => {
     const location = useLocation();
@@ -7,9 +9,25 @@ export const OrderPage = () => {
     console.log(location);
 
     return (
-        <div>
-            { userName ? <h1>Bonjour {userName}</h1> : <h1>Bonjour</h1> }
-            <Link to={`/`}>Logout</Link>
-        </div>
+        <OrderPageStyled>
+            <Navbar userName={userName}/>
+            <div className="pannel"></div>
+        </OrderPageStyled>
     )
 }
+const OrderPageStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: ${theme.colors.primary};
+    padding: 24px 56px;
+    height: calc(100vh - 48px);
+
+    .pannel {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        background-color: ${theme.colors.lightgray};
+        border-radius: 0 0 ${theme.borderRadius.round} ${theme.borderRadius.round};
+        box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+    }
+`;
