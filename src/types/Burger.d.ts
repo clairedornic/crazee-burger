f69178f8d5de07465d5e44b3ecf84772bd1d4f9a
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-17 18:04:59 +0200

    feat: add burger type

---
+++ src/types/Burger.d.ts
@@ -0,0 +1,9 @@
+interface Burger {
+    id: number;
+    imageSource: string;
+    title: string;
+    price: number;
+    quantity: number;
+    isAvailable: boolean;
+    isAdvertised: boolean;
+}
