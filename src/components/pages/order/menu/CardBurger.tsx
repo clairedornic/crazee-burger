bd23c0f93e4c3ad0210b02a26ed473bdf7b0843c
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-17 18:19:52 +0200

    feat: add text truncation

--- src/components/pages/order/menu/CardBurger.tsx
+++ src/components/pages/order/menu/CardBurger.tsx
@@ -35,7 +35,7 @@ const CardBurgerStyled = styled.div`
   border-radius: ${theme.borderRadius.extraRound};
   background: ${theme.colors.white};
   box-shadow: -8px 8px 20px 0px rgba(0, 0, 0, 0.20);
-  padding: 50px 25px 10px 25px;
+  padding: 50px 25px 30px 25px;
 
   img {
     width: 100%;
@@ -46,19 +46,27 @@ const CardBurgerStyled = styled.div`
   .content {
     display: flex;
     flex-direction: column;
-    gap: 8px;
+    gap: 15px;
     
     .title {
       font-size: ${theme.fonts.size.P4};
       font-family: ${theme.fonts.family.title};
       font-weight: ${theme.fonts.weights.bold};
       margin: 0;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
     }
 
     .bottom-card {
       display: flex;
       align-items: center;
       justify-content: space-between;
+
+      .price {
+        margin: 0;
+        color: ${theme.colors.primary};
+      }
 
       button {
         width: fit-content;
