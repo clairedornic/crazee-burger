f31061491090f9f7b3f0464080b54d32c8ba6642
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-19 16:24:48 +0200

    feat: add a toggle button for admin mode and a toast to alert the user of the change

--- src/components/pages/order/Navbar/RightSide.tsx
+++ src/components/pages/order/Navbar/RightSide.tsx
@@ -1,15 +1,59 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { Profile } from './Profile';
+import { theme } from '../../../../assets/styles/theme/theme-design';
+import { toast } from 'react-toastify';
+import { ToggleButton } from '../../../reusable-ui/ToggleButton/ToggleButton.tsx';
+import { FcInfo } from 'react-icons/fc';
 
 export const RightSide = () => {
+
+  const [checked, setChecked] = useState(false);
+
+  const handleToggleAdminButton = () => {
+    if ( !checked ) {
+      toast.info("Mode admin activé", {
+        icon: <FcInfo size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
+    setChecked(!checked);
+  }
+
   return (
     <RightSideStyled>
+        <ToggleButton isChecked={checked} onToggle={handleToggleAdminButton} labelIfChecked="Désactiver le mode admi" labelIfUnchecked="Activer le mode adminn"/>
         <Profile/>
     </RightSideStyled>
   )
 }
 
 const RightSideStyled = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
+
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.black};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `;
