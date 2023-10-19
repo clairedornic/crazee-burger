8897341cfa2cdc7a86cec8c05fe3969800e9a1e2
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-19 10:59:55 +0200

    refactor: remove useless type

--- src/types/Burger.d.ts
+++
@@ -1,9 +0,0 @@
-interface Burger {
-    id: number;
-    imageSource: string;
-    title: string;
-    price: number;
-    quantity: number;
-    isAvailable: boolean;
-    isAdvertised: boolean;
-}
