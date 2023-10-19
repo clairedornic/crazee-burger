dd2ab48be52262db5855033de0fe516991c3cd54
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-19 16:23:37 +0200

    feat: add toast container for the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from './components/pages/login/LoginPage'
 import { ErrorPage } from './components/pages/error/ErrorPage';
 import { OrderPage } from './components/pages/order/OrderPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/order" element={<OrderPage />}></Route>
         <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
     </>
   )
 }
