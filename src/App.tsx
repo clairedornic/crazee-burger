4c6157b24f81df5dc3a126a02a229c5e79383717
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-19 22:40:30 +0200

    refactor: remove toast container from app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from './components/pages/login/LoginPage'
 import { ErrorPage } from './components/pages/error/ErrorPage';
 import { OrderPage } from './components/pages/order/OrderPage';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Route path="/order" element={<OrderPage />}></Route>
         <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
-      <ToastContainer className="toaster" bodyClassName="body-toast" />
     </>
   )
 }
