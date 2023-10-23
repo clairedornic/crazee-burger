03815897a6a7ce431c5244d2e3ac8914a7e45ca8
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-23 17:35:32 +0200

    feat: add admin panel with tabs, tab, tab title and tab content

---
+++ src/components/pages/order/Admin/AdminTab.tsx
@@ -0,0 +1,12 @@
+interface AdminTabsProps {
+    title?: string
+    icon: React.ReactNode;
+}
+
+export const AdminTab: React.FC<AdminTabsProps> = ({title}) => {
+  return (
+    <div>
+        <p>{title}</p>
+    </div>
+  )
+}
---
+++ src/components/pages/order/Admin/AdminTabTitle.tsx
@@ -0,0 +1,18 @@
+export interface AdminTabTitleProps {
+    tabKey: number;
+    title: string;
+    icon?: React.ReactNode;
+    setSelectedTab : (value: number) => void;
+}
+
+
+export const AdminTabTitle: React.FC<AdminTabTitleProps> = ({tabKey, title, icon, setSelectedTab}) => {
+
+    const handleChangeOfTab = () => {
+        console.log(tabKey);
+        setSelectedTab(tabKey);
+    }
+  return (
+    <li><button onClick={handleChangeOfTab}>{icon} {title}</button></li>
+  )
+}
