ef98d9efea57904816bb020114823370b28164c4
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-24 22:05:28 +0200

    refactor: fresh start for admin tabs

--- src/components/pages/order/Main/Admin/AdminTabs.tsx
+++ src/components/pages/order/Main/Admin/AdminTabs.tsx
@@ -1,43 +1,13 @@
-import { ReactElement, useState } from "react";
 import styled from "styled-components";
-import { AdminTabTitle, AdminTabTitleProps } from "./AdminTabTitle"
-import { theme } from "../../../../../assets/styles/theme/theme-design";
 
-interface AdminTabsProps {
-    children: ReactElement<AdminTabTitleProps>[];
-}
-
-export const AdminTabs: React.FC<AdminTabsProps> = ({children}) => {
-
-    const [selectedTab, setSelectedTab] = useState<number>(1);
-
-    const isTabActive = (idTab:number, selectedTab:number) => {
-        return idTab === selectedTab;
-    }
-
-    return (
-        <AdminTabsStyled>
-            <ul>
-                {children.map((item,index) => (
-                    <AdminTabTitle tabKey={index} key={index} title={item.props.title} icon={item.props.icon} isActive={isTabActive(index, selectedTab)} setSelectedTab={setSelectedTab}/>
-                ))}
-            </ul>
-            {children[selectedTab]}
-        </AdminTabsStyled>
-    )
+export const AdminTabs = () => {
+  return (
+    <AdminTabsStyled>
+        AdminTabs
+    </AdminTabsStyled>
+  )
 }
 
 const AdminTabsStyled = styled.div`
-    height: 100%;
-
-    ul {
-        display: flex;
-        list-style: none;
-        margin: 0;
-    }
-
-    & > div {
-        border: 1px solid ${theme.colors.extraLightGray};
-        background: ${theme.colors.white};
-    }
+  
 `;
