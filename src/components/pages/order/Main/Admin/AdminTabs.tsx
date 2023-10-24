eefada86a0f99ffd0277558620d43616d97706b8
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-24 22:44:18 +0200

    feat: add tabs to AdminTabs

--- src/components/pages/order/Main/Admin/AdminTabs.tsx
+++ src/components/pages/order/Main/Admin/AdminTabs.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
+import { Tab } from "../../../../reusable-ui/Tab/Tab";
+import { AiOutlinePlus } from 'react-icons/ai';
+import { MdModeEditOutline } from 'react-icons/md';
+import { FiChevronUp } from 'react-icons/fi';
 
 export const AdminTabs = () => {
   return (
     <AdminTabsStyled>
-        AdminTabs
+        <Tab title="" icon={<FiChevronUp/>}></Tab>
+        <Tab title="Ajouter un produit" icon={<AiOutlinePlus/>}></Tab>
+        <Tab title="Modifier un produit" icon={<MdModeEditOutline/>}></Tab>
     </AdminTabsStyled>
   )
 }
