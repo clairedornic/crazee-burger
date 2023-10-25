621634bfe5eedb5583e42566d5a7357563e72642
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-25 16:15:24 +0200

    refactor: add new style and improve component content structure

--- src/components/reusable-ui/Tab/Tab.tsx
+++ src/components/reusable-ui/Tab/Tab.tsx
@@ -9,17 +9,41 @@ interface TabProps {
 export const Tab: React.FC<TabProps> = ({title, icon}) => {
   return (
     <TabContentStyled>
-       {/* <button 
-          className={isActive ? 'active' : ''}
-          onClick={handleChangeOfTab}
-        >
-          {icon} {title}
-        </button> */}
-          {icon} {title}
+          {icon} { title && <span>{title}</span> }
     </TabContentStyled>
   )
 }
 
 const TabContentStyled = styled.button`
-  height: 43px;
+    display: flex;
+    align-items: center;
+    height: 43px;
+    padding: 0px 22px;
+    border-radius: ${theme.borderRadius.round};
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+    border: 1px solid ${theme.colors.extraLightGray};
+    border-bottom: 2px solid ${theme.colors.extraLightGray};
+    background: ${theme.colors.white};
+    box-shadow: ${theme.shadows.subtle};
+    font-size: ${theme.fonts.size.P0};
+    color: ${theme.colors.mediumGray};
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      border-bottom: 2px solid ${theme.colors.white};
+      transition: all 0.2s ease-in-out;
+
+      span {
+        text-decoration: underline;
+        text-underline-offset: 4px;
+        text-decoration-thickness: 2px;
+        transition: all 0.2s ease-in-out;
+      }
+    }
+    span {
+      padding-left: 18px;
+      transition: all 0.2s ease-in-out;
+    }
 `;
