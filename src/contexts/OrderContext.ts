3f8e23e32974c5c84db500c5e0840f8b4b8a4549
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-25 19:16:29 +0200

    feat: add new state to order context to handle click on the tabs

--- src/contexts/OrderContext.ts
+++ src/contexts/OrderContext.ts
@@ -1,11 +1,22 @@
-import React from 'react';
-import {createContext} from 'react';
+import React from "react";
+import { createContext } from "react";
 
 interface OrderContextProps {
-    isModeAdmin: boolean;
-    setIsModeAdmin: React.Dispatch<React.SetStateAction<boolean>>;
+  isModeAdmin: boolean;
+  setIsModeAdmin: React.Dispatch<React.SetStateAction<boolean>>;
+  isEditSelected: boolean;
+  setIsEditSelected: React.Dispatch<React.SetStateAction<boolean>>;
+  isAddSelected: boolean;
+  setIsAddSelected: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const OrderContext = createContext<OrderContextProps | undefined>(undefined);
+const OrderContext = createContext<OrderContextProps | undefined>({
+  isModeAdmin: false,
+  setIsModeAdmin: () => {},
+  isEditSelected: false,
+  setIsEditSelected: () => {},
+  isAddSelected: false,
+  setIsAddSelected: () => {},
+});
 
 export default OrderContext;
