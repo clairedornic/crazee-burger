ba9a4e0f8679ee1ad0b27b996de969a70c3f7f38
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-26 16:49:29 +0200

    feat: add getTabSelected function

--- src/components/pages/order/Main/Admin/tabsconfig.tsx
+++ src/components/pages/order/Main/Admin/tabsconfig.tsx
@@ -1,31 +1,22 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdModeEditOutline } from "react-icons/md";
 
-interface TabsConfig {
-  index: string;
-  label: string;
-  className: string;
-  Icon: React.ReactNode;
-}
-
-export const getTabsConfig = (currentTabSelected: string): TabsConfig[] => [
-  // {
-  //   index: "panelUpDown",
-  //   label: "",
-  //   Icon: isCollapsed ? <FiChevronDown /> : <FiChevronUp />,
-  //   onClick: () => setIsCollapsed(!isCollapsed),
-  //   className: !isCollapsed ? "is-active" : "",
-  // },
+export const getTabsConfig = (): TabsConfig[] => [
   {
     index: "add",
     label: "Ajouter un produit",
-    className: currentTabSelected === "add" ? "is-active" : "",
     Icon: <AiOutlinePlus />,
   },
   {
     index: "edit",
     label: "Modifier un produit",
-    className: currentTabSelected === "edit" ? "is-active" : "",
     Icon: <MdModeEditOutline />,
   },
 ];
+
+export const getTabSelected = (
+  tabs: TabsConfig[],
+  currentTabSelected: string,
+) => {
+  return tabs.find((tab) => tab.index === currentTabSelected);
+};
