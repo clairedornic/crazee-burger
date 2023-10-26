c9ae1de532bd6ff2a2238351f4fc9c71d059dae1
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-10-26 10:17:55 +0200

    refactor: prettier the file

--- src/components/pages/order/Main/Admin/AdminPanel.tsx
+++ src/components/pages/order/Main/Admin/AdminPanel.tsx
@@ -1,19 +1,14 @@
 import { theme } from "../../../../../assets/styles/theme/theme-design";
 import styled from "styled-components";
 
-
 export const AdminPanel = () => {
-  return (
-    <AdminPanelStyled>
-        ADMIN PANEL
-    </AdminPanelStyled>
-  )
-}
+  return <AdminPanelStyled>ADMIN PANEL</AdminPanelStyled>;
+};
 
 const AdminPanelStyled = styled.div`
-    height: 250px;
-    z-index: 1;
-    border: 1px solid ${theme.colors.extraLightGray};
-    background: ${theme.colors.white};
-    box-shadow: ${theme.shadows.subtle};
+  height: 250px;
+  z-index: 1;
+  border: 1px solid ${theme.colors.extraLightGray};
+  background: ${theme.colors.white};
+  box-shadow: ${theme.shadows.subtle};
 `;
