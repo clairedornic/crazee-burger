3a678c263d0e6826354176dcbd6c1c6580e10130
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-11-23 10:53:34 +0100

    refactor: prettier admin tab

--- src/components/pages/order/Admin/AdminTab.tsx
+++ src/components/pages/order/Admin/AdminTab.tsx
@@ -1,12 +1,12 @@
 interface AdminTabsProps {
-    title?: string
-    icon: React.ReactNode;
+  title?: string;
+  icon: React.ReactNode;
 }
 
-export const AdminTab: React.FC<AdminTabsProps> = ({title}) => {
+export const AdminTab: React.FC<AdminTabsProps> = ({ title }) => {
   return (
     <div>
-        <p>{title}</p>
+      <p>{title}</p>
     </div>
-  )
-}
+  );
+};
