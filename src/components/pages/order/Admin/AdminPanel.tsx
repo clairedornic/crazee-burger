36d37e17b7a39026e1e9ea9ae65e2778ab4d9de8
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-11-23 10:52:21 +0100

    refactor: prettier admin panel

--- src/components/pages/order/Admin/AdminPanel.tsx
+++ src/components/pages/order/Admin/AdminPanel.tsx
@@ -1,28 +1,34 @@
 import styled from "styled-components";
 import { AdminTabs } from "./AdminTabs";
 import { AdminTab } from "./AdminTab";
-import { AiOutlinePlus } from 'react-icons/ai';
-import { MdModeEditOutline } from 'react-icons/md';
-import { FiChevronUp } from 'react-icons/fi';
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
+import { FiChevronUp } from "react-icons/fi";
 
 export const AdminPanel = () => {
   return (
     <AdminPanelStyled>
-        <div className="admin-container">
-            <AdminTabs>
-                <AdminTab title="" icon={<FiChevronUp/>}></AdminTab>
-                <AdminTab title="Ajouter un produit" icon={<AiOutlinePlus/>}></AdminTab>
-                <AdminTab title="Modifier un produit" icon={<MdModeEditOutline/>}></AdminTab>
-            </AdminTabs>
-        </div>
+      <div className="admin-container">
+        <AdminTabs>
+          <AdminTab title="" icon={<FiChevronUp />}></AdminTab>
+          <AdminTab
+            title="Ajouter un produit"
+            icon={<AiOutlinePlus />}
+          ></AdminTab>
+          <AdminTab
+            title="Modifier un produit"
+            icon={<MdModeEditOutline />}
+          ></AdminTab>
+        </AdminTabs>
+      </div>
     </AdminPanelStyled>
-  )
-}
+  );
+};
 
 const AdminPanelStyled = styled.div`
-    position: absolute;
-    bottom: 0;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
